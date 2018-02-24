9431dc931c6c8b7f07580a9ed15fb6dd1f4bbe08
byronhsu <byronhsu1230@gmail.com>
2018-02-25 00:51:14 +0800

    ✅ refactor 076

--- python/076-minimum-window-substring.py
+++ python/076-minimum-window-substring.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def minWindow(self, s, t):
         """
@@ -6,39 +7,41 @@ class Solution:
         :rtype: str
         """
         dic = {}
-        for i in range(len(t)):
-            if t[i] not in dic:
-                dic[t[i]] = [1, 0, []]
+        for i in t:
+            if i not in dic:
+                dic[i] = [1, deque()]
             else:
-                dic[t[i]][0] += 1
-
+                dic[i][0] += 1
+        # print(dic)
         ran = []
-        rest = len(t)
-
         for i in range(len(s)):
             if s[i] in dic:
-                dic[s[i]][2].append(i)
                 if dic[s[i]][0] > 0:
                     dic[s[i]][0] -= 1
-                    rest -= 1
+                    dic[s[i]][1].append(i)
                 else:
-                    dic[s[i]][1] += 1
-            if rest == 0:
-                l, r = 2**32, i
+                    dic[s[i]][1].popleft()
+                    dic[s[i]][1].append(i)
+            # print(dic)
+            for key in dic:
+                if dic[key][0] != 0:
+                    break
+            else:
+                l, r = 2**32, -2**32
                 for key in dic:
-                    lidx = dic[key][1]
-                    l = min(l, dic[key][2][lidx])
-
+                    l = min(l, dic[key][1][0])
+                    r = max(r, dic[key][1][-1])
                 if len(ran) == 0:
                     ran = [l, r]
                 elif r - l < ran[1] - ran[0]:
                     ran[0], ran[1] = l, r
+                # print(dic)
+                # print(l, r)
+                # print(ran)
 
+        # print(ran)
         if len(ran) == 0:
             return ""
         else:
             return s[ran[0]:ran[1]+1]
                 
-
-if __name__ == '__main__':
-    print(Solution().minWindow('acbbaca', 'aba'))
